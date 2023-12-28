e76897b0d5c4085d9014409c590cc17c0d22e82a
nico <nico@gmail.com>
2023-12-28 23:54:16 +0800

    feat(LogManager): 新增日志功能

---
+++ Swifthub/Managers/LibsManager/LibsManager.swift
@@ -0,0 +1,12 @@
+//
+//  LibsManager.swift
+//  Swifthub
+//
+//  Created by apple on 2023/12/28.
+//
+
+import UIKit
+
+class LibsManager: NSObject {
+
+}
---
+++ Swifthub/Managers/LogManager/LogManager.swift
@@ -0,0 +1,8 @@
+//
+//  LogManager.swift
+//  Swifthub
+//
+//  Created by apple on 2023/12/28.
+//
+
+import Foundation
